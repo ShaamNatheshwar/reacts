ce2de4d88bb4532fe163061b8a17a6deecf5b183
ssn <shaamnatheshwar@gmail.com>
2023-05-03 16:29:26 +0530

    here comes the trickier part

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,8 +3,11 @@ import Button from '../button/button.component';
 import { useContext } from "react";
 import { CartContext } from "../../context/cart.context";
 import CartItem from "../cart-item/cart-item.component";
+import { useNavigate } from "react-router-dom";
 const CartDropdown = ()=>{
-    const {cartItems} = useContext(CartContext)
+    const {cartItems} = useContext(CartContext);
+    const navigate = useNavigate();
+    const goToCheckout = ()=> navigate('/checkout')
     return(
         <div className="cart-dropdown-container">
             <div className="cart-items">
@@ -14,7 +17,7 @@ const CartDropdown = ()=>{
                     ))
                 }
             </div>
-            <Button>Go To Checkout</Button>
+            <Button onClick={goToCheckout}>Go To Checkout</Button>
         </div>
     )
 }
